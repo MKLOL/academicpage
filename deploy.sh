6f0c2df6b3fd98ef228ce66e5843feac2a94c75b
Dragos Ristache <ristachedragos@gmail.com>
2022-10-08 21:57:55 -0400

    Fixes

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 bundle exec jekyll build
-rm -rf var/www/example.com/
+rm -rf /var/www/example.com/
+mkdir /var/www/example.com/
+cp images/favicon* ./_site/
 cp -r ./_site/* /var/www/example.com/
-cp -r ./assets/ /var/www/example.com/
-cp -r ./images/ /var/www/example.com/
