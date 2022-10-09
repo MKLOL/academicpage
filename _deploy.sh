cb895f32846fe00fec11f0b82eb0184d27e1a47b
Dragos Ristache <ristachedragos@gmail.com>
2022-10-08 22:29:09 -0400

    Fixes more

--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,8 @@
+cp _config.yml _config_bk.yml
 sed -i 's/http:\/\/localhost:4000/https:\/\/ristache\.com/g' _config.yml
 bundle exec jekyll build
 rm -rf /var/www/example.com/
-cp _config.yml _config_bk.yml
 mkdir /var/www/example.com/
 cp images/favicon* ./_site/
 cp -r ./_site/* /var/www/example.com/
-#mv _config_bk.yml _config.yml
+mv _config_bk.yml _config.yml
