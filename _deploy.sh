5fd5bd4187439198529193e2e2b0c2173ee13a8c
Dragos Ristache <ristachedragos@gmail.com>
2023-07-31 21:14:52 -0400

    modifies deploy script

--- _deploy.sh
+++ _deploy.sh
@@ -5,5 +5,5 @@ rm -rf /var/www/example.com/
 mkdir /var/www/example.com/
 cp images/favicon* ./_site/
 cp -r ./_site/* /var/www/example.com/
-cp -r /secret/* /var/www/example.com/
+cp -r secret/* /var/www/example.com/
 mv _config_bk.yml _config.yml
